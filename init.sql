15c5c9321aa98ec0ea43bb214e57ba6c65490a4d
José Lira <joserobertolira@gmail.com>
2023-10-08 11:27:32 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'unixlira'@'%' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON dream_team.* TO 'unixlira'@'%';
+CREATE DATABASE IF NOT EXISTS `dream_team`;
+GRANT ALL ON `dream_team`.* TO 'unixlira'@'%' IDENTIFIED BY 'secret';
 FLUSH PRIVILEGES;
