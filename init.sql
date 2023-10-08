80a2fcb3b60e7e2437a00ee4261298d1350b483a
José Lira <joserobertolira@gmail.com>
2023-10-08 12:01:31 -0300

    Update init.sql2

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `dream_team`;
+CREATE USER 'unixlira'@'%' IDENTIFIED BY 'secret';
 GRANT ALL ON `dream_team`.* TO 'unixlira'@'%' IDENTIFIED BY 'secret';
 FLUSH PRIVILEGES;
